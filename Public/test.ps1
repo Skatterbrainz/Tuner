7a7d277c62234a2f218920e355ca6ffb095518ae
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-02-22 10:47:33 -0500

    Delete test.ps1

--- Public/test.ps1
+++
@@ -1,5 +0,0 @@
-$Configuration = "basic"
-$PkgListPath = Join-Path -Path $(Split-Path $PSScriptRoot) -ChildPath "Assets"
-Write-Host "configuration files path: $PkgListPath"
-$pkglist = Get-Content -Path (Join-Path $PkgListPath -ChildPath "$Configuration.txt")
-$pkglist
